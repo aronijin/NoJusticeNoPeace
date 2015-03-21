f755f65e4a68b511d117e183f1d8f5cb1ecc0e19
aronijin <aronijin@gmail.com>
2015-03-21 02:05:40 -0400

    Added toggle status flags.

--- app/src/main/java/com/studios/entropy/nojusticenopeace/NJNPActivity.java
+++ app/src/main/java/com/studios/entropy/nojusticenopeace/NJNPActivity.java
@@ -1,12 +1,20 @@
 package com.studios.entropy.nojusticenopeace;
 
+import android.media.MediaRecorder;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.ToggleButton;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  *
@@ -15,10 +23,26 @@ import android.widget.ToggleButton;
  */
 public class NJNPActivity extends ActionBarActivity {
 
+    private static final String NJNP_TAG = "NJNPActivity";
+    private static final String DIRECTORY_PATH = "/sdcard/NoJusticeNoPeace/";
+    private static final String AUDIO_FILE_NAME = "audio_file_";
+    private static final String DATE_FORMAT = "yyyy_MM_dd";
+
+    private static boolean audioStatus;
+    private static boolean videoStatus;
+    private static boolean smsStatus;
+    private static boolean emailStatus;
+    private static boolean dropboxStatus;
+    private static boolean localStatus;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_njnp);
+
+        // Create directory
+        File NJNPDirectory = new File(DIRECTORY_PATH);
+        NJNPDirectory.mkdirs();
 
         // Grab UI components
         Switch audioToggleBtn = (Switch) findViewById(R.id.audio_toggle_btn);
@@ -64,7 +88,8 @@ public class NJNPActivity extends ActionBarActivity {
     View.OnClickListener onAudioToggle = new View.OnClickListener() {
         public void onClick(View view) {
             boolean audioOn = ((Switch) view).isChecked();
-
+            audioStatus = audioOn;
+            Log.i(NJNP_TAG, "Audio: " + audioStatus);
             if (audioOn) {
                 // Enable vibrate
             } else {
@@ -76,6 +101,7 @@ public class NJNPActivity extends ActionBarActivity {
     View.OnClickListener onVideoToggle = new View.OnClickListener() {
         public void onClick(View view) {
             boolean videoOn = ((Switch) view).isChecked();
+            videoStatus = videoOn;
 
             if (videoOn) {
                 // Enable vibrate
@@ -88,6 +114,7 @@ public class NJNPActivity extends ActionBarActivity {
     View.OnClickListener onSMSToggle = new View.OnClickListener() {
         public void onClick(View view) {
             boolean smsOn = ((Switch) view).isChecked();
+            smsStatus = smsOn;
 
             if (smsOn) {
                 // Enable vibrate
@@ -100,6 +127,7 @@ public class NJNPActivity extends ActionBarActivity {
     View.OnClickListener onEmailToggle = new View.OnClickListener() {
         public void onClick(View view) {
             boolean emailOn = ((Switch) view).isChecked();
+            emailStatus = emailOn;
 
             if (emailOn) {
                 // Enable vibrate
@@ -112,9 +140,11 @@ public class NJNPActivity extends ActionBarActivity {
     View.OnClickListener onDropboxToggle = new View.OnClickListener() {
         public void onClick(View view) {
             boolean dropboxOn = ((Switch) view).isChecked();
+            dropboxStatus = dropboxOn;
 
             if (dropboxOn) {
                 // Enable vibrate
+                //TODO implement dropbox login
             } else {
                 // Disable vibrate
             }
@@ -124,6 +154,7 @@ public class NJNPActivity extends ActionBarActivity {
     View.OnClickListener onKeepOnDeviceToggle = new View.OnClickListener() {
         public void onClick(View view) {
             boolean localOn = ((Switch) view).isChecked();
+            localStatus = localOn;
 
             if (localOn) {
                 // Enable vibrate
@@ -135,13 +166,77 @@ public class NJNPActivity extends ActionBarActivity {
 
     View.OnClickListener onStartToggle = new View.OnClickListener() {
         public void onClick(View view) {
-            boolean startOn = ((Switch) view).isChecked();
+            boolean startOn = ((ToggleButton) view).isChecked();
 
+            Log.i(NJNP_TAG, "Start: " + startOn);
             if (startOn) {
                 // Enable vibrate
+                runAudio();
+                runVideo();
+                runSMS();
+                runEmail();
+                runDropbox();
+                runKeepOnDevice();
             } else {
                 // Disable vibrate
             }
         }
     };
+
+    private void runAudio() {
+        if (audioStatus) {
+            //TODO implement audio recording
+            MediaRecorder recorder = new MediaRecorder();
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT, Locale.US);
+
+            recorder.setOutputFile(DIRECTORY_PATH + AUDIO_FILE_NAME + sdf.format(new Date()) + ".3gp");
+            try {
+                recorder.prepare();
+            } catch (IOException e) {
+                Log.e(NJNP_TAG, "Error on prepare record: " + e.getMessage());
+            }
+            recorder.start();   // Recording is now started
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                Log.e(NJNP_TAG, "Error on thread sleep: " + e.getMessage());
+            }
+            recorder.stop();
+            recorder.reset();   // You can reuse the object by going back to setAudioSource() step
+            recorder.release();
+        }
+    }
+
+    private void runVideo() {
+        if (videoStatus) {
+            //TODO implement video recording
+        }
+    }
+
+    private void runSMS() {
+        if (smsStatus) {
+            //TODO implement sms sending
+        }
+    }
+
+    private void runEmail() {
+        if (emailStatus) {
+            //TODO implement email sending
+        }
+    }
+
+    private void runDropbox() {
+        if (dropboxStatus) {
+            //TODO implement dropbox saving
+        }
+    }
+
+    private void runKeepOnDevice() {
+        if (localStatus) {
+            //TODO implement local saving
+        }
+    }
 }
